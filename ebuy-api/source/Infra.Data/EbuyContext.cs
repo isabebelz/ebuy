848e91132108ef083d681e4f9101e6c4d27b177b
Isabel Oliveira <isabel.oliveira@modalgr.io>
2025-03-24 17:27:49 -0300

    Implements the ebuy context class

---
+++ ebuy-api/source/Infra.Data/EbuyContext.cs
@@ -0,0 +1,102 @@
+﻿using ebuy.Domain.SeedWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ebuy.Infra.Data
+{
+    public class EbuyContext : DbContext, IUnitOfWork 
+    {
+        private IDbContextTransaction? _currentTransaction;
+
+        public EbuyContext(DbContextOptions<EbuyContext> options) : base (options) { }
+
+        public IDbContextTransaction? GetCurrentTransaction() => _currentTransaction; 
+
+        public bool HasActiveTransaction => _currentTransaction != null;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(EbuyContext).Assembly);
+            base.OnModelCreating(builder);
+        }
+
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            _ = await base.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+
+        public async Task<IDbContextTransaction?> BeginTransactionAsync()
+        {
+            if (_currentTransaction != null) return null;
+
+            _currentTransaction = await Database.BeginTransactionAsync();
+
+            return _currentTransaction;
+        }
+
+        public async Task CommitTransactionAsync(IDbContextTransaction transaction)
+        {
+            if (transaction == null) throw new ArgumentNullException(nameof(transaction));
+            if (transaction != _currentTransaction) throw new InvalidOperationException($"Transaction {transaction.TransactionId} is not current");
+            
+            try
+            {
+                await SaveChangesAsync();
+                await transaction.CommitAsync();
+;           }
+            catch
+            {
+                RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                if (HasActiveTransaction)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public void RollbackTransaction()
+        {
+            try
+            {
+                _currentTransaction?.Rollback();
+            }
+            finally
+            {
+                if (HasActiveTransaction)
+                {
+                    _currentTransaction?.Dispose();
+                    _currentTransaction = null;
+
+                }
+            }
+        }
+
+        public async Task<bool> Commit()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("RegistrationDate") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("RegistrationDate").CurrentValue = DateTime.UtcNow;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("RegistrationDate").IsModified = false;
+                }
+            }
+
+            var success = await base.SaveChangesAsync() > 0;
+
+            return success;
+        }
+
+    }
+}
